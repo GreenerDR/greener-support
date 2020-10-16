5391f4b59af069511a4227e86e2855d84a5b33bf
Axcel Espinal <hyrulelink8zelda@gmail.com>
2020-10-16 18:57:52 -0400

    Created Tickets Component

---
+++ src/Components/Tickets/Tickets.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./TicketsPage.styles.scss"
+
+export default function TicketsPage() {
+    return (
+        <>
+            <section className="jumbotron">
+                <h1 className="display-6">Tickets<Link to="/support" className="btn createbtn">Crear</Link></h1>
+                <table class="table table-bordered table-light">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Mensaje</th>
+                            <th scope="col">Fecha de modificación</th>
+                            <th scope="col">Fecha de creación</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td><Link to='\ticket' class="btn btn-success">Resuelto</Link></td>
+                            <td>Me da error el Log In</td>
+                            <td>15 de octubre</td>
+                            <td>23 de septiembre</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td><Link to='\ticket' type="button" class="btn btn-warning">Abierto</Link></td>
+                            <td>No puedo cerrar sesión</td>
+                            <td>15 de octubre</td>
+                            <td>14 de octubre</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </>
+    )
+}
+
